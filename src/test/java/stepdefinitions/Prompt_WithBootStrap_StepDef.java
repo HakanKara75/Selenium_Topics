cbf6c37299927f38cd6e95e963cbd8af14a1ce99
hakankara75 <hakanbatirhan@gmail.com>
2024-02-05 22:32:55 +0300

    v

--- src/test/java/stepdefinitions/Prompt_WithBootStrap_StepDef.java
+++ src/test/java/stepdefinitions/Prompt_WithBootStrap_StepDef.java
@@ -44,13 +44,14 @@ public class Prompt_WithBootStrap_StepDef {
     public void assertThePrompt() {
         getDriver().manage().deleteAllCookies();
         String url="https://the-internet.herokuapp.com";
+        String popupUrl="the-internet.herokuapp.com/basic_auth";
         getDriver().get(url);
         page.basicAuth.click();
         ReusableMethods.bekle(2);
         String username="admin";
         String password="admin";
         String expectedMessage="Congratulations! You must have the proper credentials.";
-        getDriver().get("https://"+username+":"+password+"@"+"the-internet.herokuapp.com/basic_auth");
+        getDriver().get("https://"+username+":"+password+"@"+popupUrl);
         String actualMessage = getDriver().findElement(By.cssSelector("div.example p")).getText().trim();
 
         assertEquals(expectedMessage, actualMessage);
